446d839379120569d7240453cb73427839e17e51
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-01-17 23:52:21 +0100

    App first updates

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,28 @@
+const cokiesStorage = {
+    getItem: (item)=>{
+        const cookies = document.cookie.split(';').map((cookie)=>cookie.split('')
+        ).reduce((acc, [key, value])=>({
+                ...acc,
+                [key.trim()]: value
+            })
+        );
+        return cookies[key];
+    },
+    setItem: (key, value)=>{
+        document.cookie = `${key}=${value}`;
+    }
+};
+const storageType = cookie;
+const consentPropertyName = 'jdc_consent';
+const shouldShowPopup = ()=>!storageType.getItem(consentPropertyName)
+;
+const saveToStorage = ()=>storageType.setItem(consentPropertyName, true)
+;
+window.onload = ()=>{
+    if (shouldShowPopup()) {
+        const consent = confirm('Agree to the terms and conditions of the site?');
+        if (consent) saveToStorage();
+    }
+};
+
+//# sourceMappingURL=index.816e7b21.js.map
